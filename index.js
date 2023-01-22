0fdb757e437dd549a76fd9ac5629a9a272c641b6
iLouB123 <120276978+iLouB123@users.noreply.github.com>
2023-01-23 00:25:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ function loader() {
 
   preloader.style.display = "none";
 }
+
+// SWIPER
+
+var swiper = new Swiper(".s4_container", {
+  pagination: ".swiper-pagination",
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflow: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop: true,
+});
